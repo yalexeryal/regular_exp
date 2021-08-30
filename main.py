c1d8782d11ea8c89c64eaff2b4173ad1ba18424e
Aleksandr Yailohanov <yalexer@yandex.ru>
2021-08-30 18:15:45 +0300

    Completed the loop to create an empty index in the list

--- main.py
+++ main.py
@@ -9,7 +9,15 @@ with open("phonebook_raw.csv", encoding="UTF-8") as f:
 
 new_dict = {}
 for contact in contacts_list:
-    contact[0:3] = ' '.join(contact[0:3]).split()
+    names = ' '.join(contact[0:3]).split()
+    contact[0] = names[0]
+    contact[1] = names[1]
+    if len(names) < 3:
+        contact[2] = ' '
+    else:
+        contact[2] = names[2]
+    print(contact)
+
     contact[5] = re.sub(r"(\+7|8)?[\s-]*(\(?(\d{3})\)?)[\s-]*(\d{3})[\s-]*(\d{2})[\s-]*(\d{2})", r"+7(\3)\4-\5-\6",
                         contact[5])
     contact[5] = re.sub(r"\(?доб\.?\s*(\d+)\)?", r"доб.\1", contact[5])
@@ -20,11 +28,11 @@ for contact in contacts_list:
         new_dict[name] = contact
     else:
         for i, data in enumerate(contact):
-            if data != '':
+            if data != '' and data != ' ':
                 new_dict[name][i] = data
 
-new_list = list(new_dict.values())
+#new_list = list(new_dict.values())
 
 with open("phonebook.csv", "w", encoding="UTF-8") as f:
     datawriter = csv.writer(f, delimiter=',')
-    datawriter.writerows(new_list)
+    datawriter.writerows(list(new_dict.values()))
