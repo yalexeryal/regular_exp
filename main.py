93fe92db8da386f9e712c24cb116bae4f2ee3dd0
Aleksandr Yailohanov <yalexer@yandex.ru>
2021-08-30 17:28:33 +0300

    created a regular expression to replace the phones

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 from pprint import pprint
 import csv
+import re
 
+with open("phonebook_raw.csv", encoding="UTF-8") as f:
+    rows = csv.reader(f, delimiter=",")
+    contacts_list = list(rows)
+#pprint(contacts_list)
 
-with open("phonebook_raw.csv") as f:
-  rows = csv.reader(f, delimiter=",")
-  contacts_list = list(rows)
-pprint(contacts_list)
+new_dict = {}
+for contact in contacts_list:
+    contact[0:3] = ' '.join(contact[0:3]).split()
+    contact[5] = re.sub(r"(\+7|8)?[\s-]*(\(?(\d{3})\)?)[\s-]*(\d{3})[\s-]*(\d{2})[\s-]*(\d{2})", r"+7(\3)\4-\5-\6",
+                        contact[5])
+    contact[5] = re.sub(r"\(?доб\.?\s*(\d+)\)?", r"доб.\1", contact[5])
 
-# TODO 1: выполните пункты 1-3 ДЗ
-# ваш код
+    name = contact[0] + contact[1]
 
-# TODO 2: сохраните получившиеся данные в другой файл
-# код для записи файла в формате CSV
-with open("phonebook.csv", "w") as f:
-  datawriter = csv.writer(f, delimiter=',')
-  # Вместо contacts_list подставьте свой список
-  datawriter.writerows(contacts_list)
+    if name not in new_dict:
+        new_dict[name] = contact
+    else:
+        for i, data in enumerate(contact):
+            if data != '':
+                new_dict[name][i] = data
+
+new_list = list(new_dict.values())
+
+with open("phonebook.csv", "w", encoding="UTF-8") as f:
+    datawriter = csv.writer(f, delimiter=',')
+    datawriter.writerows(new_list)
